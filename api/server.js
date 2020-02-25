9de3447b55bfb9c434245c94d2978ff9942a2961
Alexis <alexisdavalos.tech@gmail.com>
2020-02-25 14:42:12 -0500

    Finished Guided Project

--- api/server.js
+++ api/server.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexStore = require('connect-session-knex')(session); //remember to curry and pass the session
 
+const restricted = require('../auth/restricted-middleware')
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const knex = require('../database/dbConfig.js') //need for storing session in the database
 
 const server = express();
 
+const sessionConfig = {
+  name: "monster", //banana
+  secret: "keep it secret, keep it safe!",
+  resave: false,
+  saveUninitialized: true, //related to GDPR compliance
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false, // should be true in production
+    httpOnly: true // true means JS can't touch the cookie
+  },
+  // remember the new keyword for KnexStore config
+  store: new KnexStore({
+    knex,
+    tablename: 'sessions',
+    createtable: true,
+    sidfieldname: 'sid',
+    clearInterval: 1000 * 60 * 15,
+  }),
+}
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use('/api/users', restricted, usersRouter);
 
 server.get('/', (req, res) => {
+  console.log(req.session)
   res.json({ api: 'up' });
 });
 
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -11,6 +11,7 @@ router.post('/register', (req, res) => {
 
   Users.add(user)
     .then(saved => {
+      req.session.loggedIn = true;
       res.status(201).json(saved);
     })
     .catch(error => {
@@ -25,6 +26,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        req.session.username = user.username;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
@@ -37,4 +40,17 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.get("/logout", (req, res) =>{
+  if(req.session){
+    req.session.destroy(err => {
+      if(err) {
+        res.status(500).json({message: 'cannot log out'})
+      }else{
+        res.status(200).json({message: "you logged out successfully"});
+      }
+    });
+  } else{
+    res.status(200).json({message: 'goodbye!'})
+  }
+})
 module.exports = router;
